c459d5dc515a3c036992042f5bc6edfd4c66973e
Georgia <ghobs987@gmail.com>
2022-02-04 21:45:10 +0000

    apollo client

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  ApolloLink,
+  concat,
+} from "@apollo/client";
+import reportWebVitals from "./reportWebVitals";
+// uri as it is a object.
+const httpLink = new HttpLink({ uri: "/graphql" });
+// creates auth middleware checks you're authorized.
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      // spread and update header. if not logged in gets token from server.
+      ...headers,
+      authorization: Auth.loggedIn() ? Auth.getToken() : null,
+    },
+  }));
+
+  return forward(operation);
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: concat(authMiddleware, httpLink),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
