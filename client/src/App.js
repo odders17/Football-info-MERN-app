335c3ea9336f702518e6b1e13a9a69f9d9951170
Georgia <ghobs987@gmail.com>
2022-02-06 08:03:17 +0000

    components for teams

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,43 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { standingsSearch } from "./utils/searchreq";
+// import { standingsSearch } from "./utils/searchreq";
+import TeamCard from "./components/Teams"
 
 import Navbar from "./components/Navbar";
 
-function App() {
-  const [league, setLeague] = useState([]);
-  const [leagueState, setLeagueState] = useState([]);
-  useEffect(() => {
-    // async self emmitting func  cant use async on use effect call back.
-    (async () => {
-      const url = "https://api.football-data.org/v2/competitions/PL/standings";
-      const results = await standingsSearch(url);
-      console.log("resr", results);
-      setLeague(results.standings[0].table);
-    })();
-    // setData(results);
-    // only run this code when component loads.
-  }, []);
-  console.log("league ", league);
+// function App() {
+//   const [league, setLeague] = useState([]);
+//   const [leagueState, setLeagueState] = useState([]);
+//   useEffect(() => {
+//     // async self emmitting func  cant use async on use effect call back.
+//     (async () => {
+//       const url = "https://api.football-data.org/v2/competitions/PL/standings";
+//       const results = await standingsSearch(url);
+//       console.log("resr", results);
+//       setLeague(results.standings[0].table);
+//     })();
+//     // setData(results);
+//     // only run this code when component loads.
+//   }, []);
+//   console.log("league ", league);
 
-  return league.map((l) => (
-    <div>
-      {l.team.name}
-      {l.draw}
-      <img src={l.team.crestUrl} alt={l.team.name} />
-    </div>
-  ));
+//   return league.map((l) => (
+//     <div>
+//       {l.team.name}
+//       {l.draw}
+//       <img src={l.team.crestUrl} alt={l.team.name} />
+//     </div>
+//   ));
+// }
+function App() {
+
+  return (
+    <Router>
+      <Navbar />
+      <TeamCard />
+ 
+    </Router>
+  );
 }
 
 export default App;
