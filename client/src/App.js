986080ce6632867e59129e39dc74c333a077d883
Georgia <ghobs987@gmail.com>
2022-02-08 19:09:10 +0000

    mvp 1 styling and refractoring of styling ele, background img

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import TeamCard from "./components/Teams";
-
+import trophy from "./images/trophybackground.png";
 import Navbar from "./components/Navbar";
-
 function App() {
+  const backgroundstyle = {
+    backgroundImage: `url(
+      ${trophy}
+      )`,
+  };
   return (
-    <Router>
-      <Navbar />
-      <TeamCard />
-    </Router>
+    // https://tailwindcss.com/docs/background-attachment#scroll
+    <div className="bg-scroll" style={backgroundstyle}>
+      {/* "<img src={trophy} className="w-full" alt="trophy" /> */}
+      <Router>
+        <Navbar />
+        <TeamCard />
+      </Router>
+    </div>
   );
 }
 
--- client/src/components/Teams.js
+++ client/src/components/Teams.js
@@ -1,30 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { standingsSearch } from "../utils/searchreq";
 import { initialData } from "./data";
+import "./team.css";
 function TeamCard() {
   const [league, setLeague] = useState(initialData);
   const [leagueState, setLeagueState] = useState([]);
   const pos = {
-    1: "bg-green-600 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black ",
-    2: "bg-green-900 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black",
-    3: "bg-green-900 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black",
-    4: "bg-green-900 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black",
-    5: "bg-cyan-600 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black",
-    6: "bg-cyan-600 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black",
-    7: "bg-cyan-600 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black",
-    8: "bg-blue-500 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black",
-    9: "bg-blue-500 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black",
-    10: "bg-blue-900 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black",
-    11: "bg-indigo-500 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black",
-    12: "bg-indigo-500 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black",
-    13: "bg-indigo-900 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black",
-    14: "bg-indigo-900 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black",
-    15: "bg-orange-600 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black",
-    16: "bg-orange-600 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black",
-    17: "bg-orange-600 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black",
-    18: "bg-red-700 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black",
-    19: "bg-red-700 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black",
-    20: "bg-red-700 m-2 rounded-xl shadow-xl m-2 p-2 border-2 border-black",
+    1: "bg-green-600 teamStyle ",
+    2: "bg-green-900 teamStyle",
+    3: "bg-green-900 teamStyle",
+    4: "bg-green-900 teamStyle",
+    5: "bg-cyan-600 teamStyle",
+    6: "bg-cyan-600 teamStyle",
+    7: "bg-cyan-600 teamStyle",
+    8: "bg-blue-500 teamStyle",
+    9: "bg-blue-500 teamStyle",
+    10: "bg-blue-900 teamStyle",
+    11: "bg-indigo-500 teamStyle",
+    12: "bg-indigo-500 teamStyle",
+    13: "bg-indigo-900 teamStyle",
+    14: "bg-indigo-900 teamStyle",
+    15: "bg-orange-600 teamStyle",
+    16: "bg-orange-600 teamStyle",
+    17: "bg-orange-600 teamStyle",
+    18: "bg-red-700 teamStyle",
+    19: "bg-red-700 teamStyle",
+    20: "bg-red-700 teamStyle",
   };
   useEffect(() => {
     // async self emmitting func  cant use async on use effect call back.
@@ -41,23 +42,24 @@ function TeamCard() {
   console.log("league ", league);
 
   return (
-    <div className=" flex-auto m-2">
+    <div className=" container">
       {league
         ? league.map((teams, index) => (
             <div key={index} className={pos[teams.position]}>
-              <div className=" flex">
-                <p className="font-fantasy, text-3xl, text-white">
-                  {" "}
+              <div className=" flex max-w-[20%] m-0 ">
+                <h1 className="text-2xl text-white">
                   Position {teams.position}
-                </p>
+                </h1>
                 <img
                   src={teams.team.crestUrl}
                   alt={teams.team.name}
-                  className="w-20"
+                  className="w-20 m-0"
                 />
               </div>
-              <div>
-                {teams.team.name} points {teams.points}
+              <div className="max-w-sm m-0">
+                <p>
+                  {teams.team.name} points {teams.points}
+                </p>
                 <p>
                   games played {teams.playedGames} wins {teams.won} draws{" "}
                   {teams.draw} lost {teams.lost}
