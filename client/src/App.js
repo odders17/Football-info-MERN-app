04b4ea02a0b0b5028501b0f26cd9e5f2d84c5f2f
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2022-02-08 20:56:00 +0000

    add backfround

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import TeamCard from "./components/Teams";
-import trophy from "./images/trophybackground.png";
+
 import Navbar from "./components/Navbar";
+
 function App() {
-  const backgroundstyle = {
-    backgroundImage: `url(
-      ${trophy}
-      )`,
-  };
   return (
-    // https://tailwindcss.com/docs/background-attachment#scroll
-    <div className="bg-scroll" style={backgroundstyle}>
-      {/* "<img src={trophy} className="w-full" alt="trophy" /> */}
-      <Router>
-        <Navbar />
-        <TeamCard />
-      </Router>
-    </div>
+    <Router>
+      <Navbar />
+      <TeamCard />
+    </Router>
   );
 }
 
