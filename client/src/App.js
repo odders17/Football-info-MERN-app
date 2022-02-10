23a9325b84d7a96f55199d7c08fe79fe7055c897
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2022-02-10 21:20:26 +0000

    fix loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { getApi } from "./utils/queries";
 
 import TeamCard from "./components/Teams";
 import Navbar from "./components/Navbar";
@@ -10,7 +12,7 @@ function App() {
   return (
     <Router>
       {/* <Navbar /> */}
-      <TeamCard ApiKey={data["getApi"]["apiKey"]} />
+      {loading ? "...Loading" : <TeamCard ApiKEy={data["getApi"]["apiKey"]} />}
     </Router>
   );
 }
