ba580974c9187f6ed2e5ca38718ea9f91ef76352
odders17 <odders17@gmail.com>
2022-02-27 22:01:56 +0000

    added more styling

--- client/src/components/PlayerInfo.js
+++ client/src/components/PlayerInfo.js
@@ -1,8 +1,8 @@
 export function PlayerInfo({ data }, ApiKey) {
   return (
     <div className="mx-auto border-2 p-2 m-4 bg-gray-800 shadow-xl m-2 rounded-xl">
-      <span className="p-2 m-2 text-3xl text-gray-800">{data.title}:</span>
-      <span>{data.info ? data.info : "TBC"}</span>
+      <span className="p-2 m-2 text-xl md:text-3xl text-white">{data.title}:</span>
+      <span className="p-2 m-2 text-xl md:text-3xl text-black">{data.info ? data.info : "TBC"}</span>
     </div>
   );
 }
